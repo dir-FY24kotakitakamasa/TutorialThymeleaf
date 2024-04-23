02e950ed960b19d5867870f74ddf3d4721ba8dfe
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-23 12:19:26 +0900

    20-1

---
+++ TutorialThymeleaf/src/main/java/com/techacademy/MsgController.java
@@ -0,0 +1,14 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MsgController {
+@GetMapping("/msg")
+public String getMsg() {
+    return "msg";
+}
+
+}
